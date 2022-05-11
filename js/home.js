8ef2fedef3d674979a37b5064420eb4da80dde41
dkscksals12 <dkscksals12@g.skku.edu>
2022-05-12 02:34:11 +0900

    home Page

---
+++ js/home.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelector("#buttons");
+console.log("a");
+console.log(buttons);
+const signupEmail = (email,password) =>{
+    return firebase.auth().createUserWithEmailAndPassword(email,password);
+}
+const loginEmail = (email,password)=>{
+    return firebase.auth().signInWithEmailAndPassword(email,password);
+}
+const login_area = document.getElementById('login-area');
+buttons.addEventListener('click', (e) => {
+    e.preventDefault();
+    login_area.innerHTML = "";
+    email = document.querySelector("#inputEmail2");
+    password = document.querySelector("#inputPassword2")
+    if (e.target.id === 'signin') {
+      loginEmail(email.value, password.value).then((result) => {
+        console.log(result);
+        const user = result.user;
+        loginSuccess(user.email, user.uid);
+      })
+      .catch(()=> login_area.innerHTML ="아이디 또는 비밀번호가 틀렸습니다.");
+    } else if (e.target.id === 'signup') {
+      signupEmail(email.value, password.value) //
+        .then((result) => {
+          const user = result.user;
+          loginSuccess(user.email, user.uid);
+        })
+        .catch(()=> login_area.innerHTML="아이디가 이미 존재합니다.");
+    }
+  });
+  const loginSuccess = (email, uid) => {
+    login_area.innerHTML = `<h2>Login 성공!</h2><div>uid: ${uid}</div><div>email: ${email}</div>`;
+  };
+  
--- js/save.js
+++ js/save.js
@@ -1,17 +1,42 @@
+
+
 let buttonBookAdd = document.querySelector("#Add");
 let buttonCreate = document.querySelector("#create");
 let containerClick = document.querySelector("#gray-container");
 const fileInput = document.getElementById("fileUpload");
+const storage = firebase.storage();
+const db = firebase.firestore();
 let books = [];
 const Type = {
   main:0,
   list:1,
 }
+
 window.addEventListener("load",()=>{
   loadBooks();
 })
 const handleFiles = (e) =>{
   const selectedFile = fileInput.files[0];
+  const storageRef = storage.ref();
+  const uploadPath = storageRef.child('file/' + selectedFile.name);
+  const upload = uploadPath.put(selectedFile);
+  upload.on('state_changed',
+  //변화시 동작하는 함수
+  null,
+  //에러시 동작하는 함수
+  (error) =>{
+    console.error(error);
+  },
+  //성공시 동작하는 함수
+  ()=>{
+    upload.snapshot.ref.getDownloadURL().then(url=> {
+      db.collection('file').add({
+        url: url
+      }).catch((err)=>{
+        console.log(err);
+      })
+    })  
+  });
   let selectedFile_url = URL.createObjectURL(selectedFile);
   let embed = document.createElement("embed");
   embed.id = "page";
@@ -21,6 +46,9 @@ const handleFiles = (e) =>{
   list.insertBefore(embed,list.firstChild);
 };
 fileInput.addEventListener("change",handleFiles);
+function loadPages(){
+  
+}
 
 let buttonMainBook = document.querySelector("#mainbook");
 buttonMainBook.addEventListener("click",()=>{
@@ -31,23 +59,20 @@ buttonMainBook.addEventListener("click",()=>{
 buttonBookAdd.addEventListener("click",()=>{
   document.getElementById("gray-container").style.display = "block";
 })
-
 containerClick.addEventListener("click",(e)=>{
   if(e.target===document.getElementById("gray-container")) 
     e.target.style.display = "none";
 })
 function loadBooks(){
   let lastBooks = localStorage.getItem("books");
-  if(lastBooks === '0') return;
+  if(!lastBooks) return;
   if(books.find(b => b.type === 0) !== '0') document.getElementById("mainbook").remove();
   books = JSON.parse(lastBooks);
-  
   books.forEach(addToList);
 }
 function saveBooks(){
   localStorage.setItem("books", JSON.stringify(books));
 }
-
 function addToList(book){
   if(book.type === 1){
     let newBook = document.createElement("li");
