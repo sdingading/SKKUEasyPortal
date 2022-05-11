be676bae46f43cba0b20b2ff64e1393b75dfb482
dkscksals12 <dkscksals12@g.skku.edu>
2022-05-12 05:39:55 +0900

    Login and Session keep

--- js/login.js
+++ js/login.js
@@ -1,17 +1,19 @@
 const buttons = document.querySelector("#buttons");
 const db = firebase.firestore();
 const signupEmail = (email,password) =>{
-    firebase.auth().createUserWithEmailAndPassword(email,password);
+    db.collection("users").doc(email).set({
+    email : email,
+    password : password
+    });
+    return firebase.auth().createUserWithEmailAndPassword(email,password);
 }
 const loginEmail = (email,password)=>{
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-    firebase.auth().signInWithEmailAndPassword(email,password);
-    firebase.auth().setStatus(" ");
+    return firebase.auth().signInWithEmailAndPassword(email,password);
 }
 firebase.auth().onAuthStateChanged((user)=>{
-  console.log(firebase.auth().currentUser);
   if(user){
-    location.href = "Search.html";
+    location.href="Search.html";
   }
 });
 
@@ -28,12 +30,7 @@ buttons.addEventListener('click', (e) => {
       .catch(()=> login_area.innerHTML ="아이디 또는 비밀번호가 틀렸습니다.");
     } else if (e.target.id === 'signup') {
       signupEmail(email.value, password.value) //
-        .then((result) => {
-          const user = result.user;
-          db.collection("users").doc(email.value).set({
-            email : email.value,
-            password : password.value
-          });
+        .then(() => {
           login_area.innerHTML="회원가입이 완료되었습니다.";
         })
         .catch(()=> login_area.innerHTML="아이디가 이미 존재합니다.");
