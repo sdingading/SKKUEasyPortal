8f82da2a2782c79a5314b849bb4b024be4b43107
dkscksals12 <dkscksals12@g.skku.edu>
2022-05-13 03:52:29 +0900

    Fix LoginPage and Add Firebase

--- js/save.js
+++ js/save.js
@@ -1,34 +1,54 @@
 var uid = 0;
+var initialBook= 0;
+var isthere = 0;
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, uid로 사용자 파일관리
       uid = user.uid;
+      loadBooks();
       // ...
     } else {
       // User is signed out
       location.href = "login.html";
     }
-  });
+});
 
 let buttonBookAdd = document.querySelector("#Add");
 let buttonCreate = document.querySelector("#create");
 let containerClick = document.querySelector("#gray-container");
 const fileInput = document.getElementById("fileUpload");
 const storage = firebase.storage();
-const db = firebase.firestore();
+const db = firebase.database();
 let books = [];
 const Type = {
   main:0,
   list:1,
 }
 
-window.addEventListener("load",()=>{
-  loadBooks();
-})
+
+function loadBooks(){
+
+  db.ref('users/'+uid+'/books/').get().then((snap)=>{
+    if(snap.exists()){
+      books = Object.values(snap.val());
+      initialBook = 1;
+      books.forEach(addToList);
+    }
+    else{
+      document.getElementById("gray-container").style.display = "block";
+    }
+  });
+}
+function loadPages(){
+}
+function savePages(){
+}
+
+
 const handleFiles = (e) =>{
   const selectedFile = fileInput.files[0];
   const storageRef = storage.ref();
-  const uploadPath = storageRef.child('file/' + selectedFile.name);
+  const uploadPath = storageRef.child(uid + '/' + selectedFile.name);
   const upload = uploadPath.put(selectedFile);
   upload.on('state_changed',
   //변화시 동작하는 함수
@@ -38,15 +58,7 @@ const handleFiles = (e) =>{
     console.error(error);
   },
   //성공시 동작하는 함수
-  ()=>{
-    upload.snapshot.ref.getDownloadURL().then(url=> {
-      db.collection('file').add({
-        url: url
-      }).catch((err)=>{
-        console.log(err);
-      })
-    })  
-  });
+  ()=>{});
   let selectedFile_url = URL.createObjectURL(selectedFile);
   let embed = document.createElement("embed");
   embed.id = "page";
@@ -56,16 +68,8 @@ const handleFiles = (e) =>{
   list.insertBefore(embed,list.firstChild);
 };
 fileInput.addEventListener("change",handleFiles);
-function loadPages(){
-  
-}
 
 let buttonMainBook = document.querySelector("#mainbook");
-buttonMainBook.addEventListener("click",()=>{
-  let booklet = document.getElementById("booklet");
-    if(booklet.style.display === "none") booklet.style.display = "flex";
-    else booklet.style.display = "none";
-})
 buttonBookAdd.addEventListener("click",()=>{
   document.getElementById("gray-container").style.display = "block";
 })
@@ -73,16 +77,6 @@ containerClick.addEventListener("click",(e)=>{
   if(e.target===document.getElementById("gray-container")) 
     e.target.style.display = "none";
 })
-function loadBooks(){
-  let lastBooks = localStorage.getItem("books");
-  if(!lastBooks) return;
-  if(books.find(b => b.type === 0) !== '0') document.getElementById("mainbook").remove();
-  books = JSON.parse(lastBooks);
-  books.forEach(addToList);
-}
-function saveBooks(){
-  localStorage.setItem("books", JSON.stringify(books));
-}
 function addToList(book){
   if(book.type === 1){
     let newBook = document.createElement("li");
@@ -93,23 +87,39 @@ function addToList(book){
     newBook.className ="book m-1";
     newBook.addEventListener("click",(e)=>{
       if(e.target === newBook){
-
-        let booklist = books.find(b => b === book);
-        let mainbook = books.find(b => b.type === 0);
-        booklist.type = 0;
-        console.log(books);
-        document.getElementById("mainbook").remove();
-        newBook.remove();
-
-        if(mainbook){
-          mainbook.type = 1;
-          addToList(mainbook);
+        let booklist;
+        let mainbook;
+        db.ref('users/'+uid+'/books').get().then((snap)=>{
+          let bookindex = 0;
+          let mainbookindex = 0;
+          booklist = Object.values(snap.val()).find((val,i)=>{
+            if(JSON.stringify(book) === JSON.stringify(val)){
+            bookindex= i;
+            return val;
+            }})
+           db.ref('users/'+uid+'/books/'+Object.keys(snap.val())[bookindex])
+              .update({type:0})
+          mainbook = Object.values(snap.val()).find((val,i)=>{
+            if(val.type === 0){
+              mainbookindex = i;
+              return val;
+            }})
+            db.ref('users/'+uid+'/books/'+Object.keys(snap.val())[mainbookindex])
+              .update({ type:1})
+              .then(()=>{
+                booklist.type = 0;
+                document.getElementById("mainbook").remove();
+                if(mainbook){
+                  mainbook.type = 1;
+                  addToList(mainbook);
+                }
+                newBook.remove();
+                addToList(booklist)
+                document.querySelector("#booklet").style.display = "none";
+              });
+          })
         }
-
-        addToList(booklist);
-        document.querySelector("#booklet").style.display = "none";
-      }
-    })
+          })
     let icon = document.createElement("i");
     icon.id = "delete";
     icon.className = "bi bi-x fs-2"
@@ -117,12 +127,10 @@ function addToList(book){
     icon.addEventListener("click",()=>{
       newBook.remove();
       books = books.filter(b=>b !== book);
-      saveBooks();
     })
     newBook.appendChild(icon);
     let booklet = document.querySelector("#tab");
     booklet.insertBefore(newBook,booklet.firstChild); 
-    saveBooks();
   }
   else{
     let newBook = document.createElement("div");
@@ -139,7 +147,6 @@ function addToList(book){
         else booklet.style.display = "none";
     })
     container.insertBefore(newBook,container.firstChild);
-    saveBooks();
   }
 }
 
@@ -148,11 +155,16 @@ buttonCreate.addEventListener("click",()=>{
     text: `<p>${document.querySelector(".letter").innerText}</p>`,
     backgroundColor : bgColor,
     color : ftColor,
-    type : 1,
+    type : initialBook,
+    isthere : isthere,
     page :{}
   }
-  books.push(book);
-  saveBooks();
+  initialBook = 1;
   addToList(book);
+  if(book.isthere === 0){
+    book.isthere = 1;
+    books.push(book);
+    db.ref('users/'+uid+"/"+"books").push().set(book);
+  }
   document.getElementById("gray-container").style.display = "none";
 })
