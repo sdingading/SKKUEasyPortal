2e333e2999fc2928d13030b03ce27e6639d1c526
sdingading <seyoungshin990@gmail.com>
2022-05-21 05:12:43 +0900

    modify Study.css & add refresh button

--- js/study.js
+++ js/study.js
@@ -62,10 +62,16 @@ function SubjectAdd(title,time){
   //delete Button 추가
   let deleteButton = document.createElement("div");
   deleteButton.className = "deleteButton";
-  deleteButton.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6F5141" class="bi bi-x-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>'
+  deleteButton.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6F5141" class="bi bi-x-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/></svg>'
   dflex.append(deleteButton);
   
-  dflex.appendChild(div);
+   //refresh Button
+   let refreshButton = document.createElement("div");
+   refreshButton.className = "deleteButton";
+   refreshButton.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6F5141" class="bi bi-eraser-fill" viewBox="0 0 16 16"><path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"/></svg>'
+   dflex.append(refreshButton);
+
+  dflex.appendChild(div); //과목이름
 
   let button = document.createElement("button");
   button.type = "button";
@@ -76,6 +82,15 @@ function SubjectAdd(title,time){
   let autosave = null;
   let count = time;
   let curcount = 0;
+
+  deleteButton.addEventListener("click",()=>{
+    console.log('과목지우기');
+  })
+
+  refreshButton.addEventListener("click", ()=>{
+    console.log("누적시간0으로돌리기");
+  })
+
   button.addEventListener("click",()=>{
     if(clocking){
       clocking= 0;
@@ -120,6 +135,7 @@ function SubjectAdd(title,time){
       }, 1000);
       button.className = "pauseButton";
     }
+
   })
   dflex.appendChild(button);
   let sec = count;
